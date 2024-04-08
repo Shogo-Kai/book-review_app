f7a5f5c80ab905cd7b63005e707e402e4576456b
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-04-08 23:27:23 +0900

    実験

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,11 +4,11 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
 import { SignIn } from '../pages/SignIn';
-import { NewTask } from '../pages/NewTask';
-import { NewList } from '../pages/NewList';
-import { EditTask } from '../pages/EditTask';
+
+
+
 import { SignUp } from '../pages/SignUp';
-import { EditList } from '../pages/EditList';
+
 
 export const Router = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
@@ -22,10 +22,10 @@ export const Router = () => {
         {auth ? (
           <>
             <Route path="/" element={<Home />} />
-            <Route path="/task/new" element={<NewTask />} />
-            <Route path="/list/new" element={<NewList />} />
-            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
-            <Route path="/lists/:listId/edit" element={<EditList />} />
+            
+           
+            
+            
           </>
         ) : (
           <Route path="*" element={<Navigate to="/signin" />} />
